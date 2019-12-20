0b5537e622d0928ae08a03dd430589af75596e62
benigndeus <benigndeus@gmail.com>
2019-12-20 21:59:12 +0900

    google maps api test

--- src/js/widget.js
+++ src/js/widget.js
@@ -1,47 +1,53 @@
-/* Weather API
-	- 날씨 정보 제공 : https://openweathermap.org
-	- 위치 정보 제공 : https://ip-api.com
-
-	- openweathermap
-		api key를 발급받아 사용했음. 호출만 하면 JSON 파일을 바로 넘겨주기 때문에 원하는 자료를 쉽게 뽑아 쓸 수 있음. 여러 가지 방식에 대한 연습이 필요할 것으로 보임. 날씨 이미지도 제공. 지금은 도시 정보를 알아내는 방법을 몰라서 ip-api를 사용한 것임.
-
-	- ip-api
-		이건 뭐 그냥 호출만 하면 됨. 지금은 api key 필요 없는데 사실 지금 제대로 사용하고 있는지 알 수 없음. 기본적으로 ip를 통해 사용자의 대략적인 위치정보(lat, lon)를 알 수 있음. 도시의 이름까지 알아낼 수 있는데, 받아오는 내용을 확인하고 테스트해 봐야 알 수 있음.
-		** 현재 Mixed Context 현상으로 인해 http 로드는 사용할 수 없을 듯 하여 대신할 API를 찾고 있다.
-*/
 $(function() {
-	var ipinfoKey = "17f45312c140cf";
-	$.getJSON("https://ipinfo.io/json?token=" + ipinfoKey
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            initMap(lat, lng);
+        }, function() {
+            handleLocationError(true, infoWindow);
+        });
+    } else {
+        // 위치정보를 사용할 수 없는 경우 IP주소를 통한 대략적인 위치정보를 얻어낸다.
+        getPosFromIP();
+    }
+
+    function initMap(lat, lng) {
+        // 전혀 필요없는 map이지만 도시 정보를 읽어오기 위해 쓴다... 일단...
+        var map = new google.maps.Map(document.getElementById('map'),
+        { zoom: 8, center: {lat: lat, lng: lng}});
+        var geocoder = new google.maps.Geocoder;
+        geocodeLatLng(geocoder, map);
+    }
+
+    function geocodeLatLng(geocoder, map) {
+        var input = map.center.lat + "," + map.center.lng;
+        console.log(map);
+        var latlngStr = input.split(',', 2);
+        var latlng = { lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+        geocoder.geocode({'location': latlng}, function(results, status) {
+            if (status === 'OK') {
+                if (results[0]) {
+                    console.log(results[0]); //*******
+                } else {
+                    window.alert('No results found');
+                }
+            } else {
+                window.alert('Geocoder failed due to: ' + status);
+            }
+        });
+    }
+
+    function getPosFromIP() {
+        var ipinfoKey = "17f45312c140cf";
+        $.getJSON("https://ipinfo.io/json?token=" + ipinfoKey
 	,function(json) {
-		console.log(json);
+		//console.log(json);
 		$("#w-city").html(json.city);
 		var locArray = json.loc.split(",");
 		var lat = locArray[0];
 		var lng = locArray[1];
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function(position) {
-				lat = position.coords.latitude;
-				lng = position.coords.longitude;
-				getWeather(lat, lng); // 날씨 추적
-			}, function() {
-				handleLocationError(true, infoWindow);
-			});
-		} else {
-			console.log("위치정보서비스 사용 불가 -> IP주소를 통해 대략적인 위치의 날씨를 출력");
-			getWeather(lat, lng);
-		}
+        initMap(lat, lng);
 	});
-
-	function getWeather(lat, lng) { // 날씨 정보 읽어오는 부분
-		var weatherKey = "56a89a0896774bfd861d459497cc1af4";
-		$.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=" + weatherKey
-		,function(json) {
-			// console.log(json);
-			/*절대 온도 주어지므로 받은 값에서 273을 뺌*/
-			$("#w-temperature-celcius").html("기온 " + Math.round(json.main.temp - 273) + " C&deg;");
-			$("#w-humidity").html("습도 " + json.main.humidity + " %");
-			$("#w-overall").html(json.weather[0].main);
-			$("#w-icon").html('<img src="http://openweathermap.org/img/w/' + json.weather[0].icon + '.png" alt="날씨 이미지">');
-		});
-	}
+    }
 });
